22a956c8055f0936d0fe792131ffbb05d6d4e966
AiNga04 <aingaspkt@gmail.com>
2025-07-06 23:05:57 +0700

    [+] dashboard

--- booking-ui/src/app/[locale]/(admin)/admin/page.tsx
+++ booking-ui/src/app/[locale]/(admin)/admin/page.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+'use client';
 import '@/assets/globals.css';
 
-const AdminPage = () => {
+import { ChartAreaInteractive } from '@/components/chart-area-interactive';
+import { DataTable } from '@/components/data-table';
+import { SectionCards } from '@/components/section-cards';
+
+import data from './data.json';
+
+export default function Page() {
   return (
-    <div className="min-h-screen full-screen bg-[#fff] p-4">
-      <h1 className="text-center text-2xl">Admin Page</h1>
+    <div className="flex flex-1 flex-col">
+      <div className="@container/main flex flex-1 flex-col gap-2">
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+          <SectionCards />
+          <div className="px-4 lg:px-6">
+            <ChartAreaInteractive />
+          </div>
+          <DataTable data={data} />
+        </div>
+      </div>
     </div>
   );
-};
-
-export default AdminPage;
+}
--- booking-ui/src/app/[locale]/(admin)/layout.tsx
+++ booking-ui/src/app/[locale]/(admin)/layout.tsx
@@ -1,15 +1,25 @@
-import { SidebarProvider } from '@/components/ui/sidebar';
-import { AppSidebar } from '@/components/admin/app-sidebar';
-import { SiteHeader } from '@/components/admin/site-header';
+'use client';
+import '@/assets/globals.css';
+
+import { AppSidebar } from '@/components/app-sidebar';
+import { SiteHeader } from '@/components/site-header';
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <main>
+    <SidebarProvider
+      style={
+        {
+          '--sidebar-width': 'calc(var(--spacing) * 72)',
+          '--header-height': 'calc(var(--spacing) * 12)',
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
         <SiteHeader />
-        {children}
-      </main>
+        <main>{children}</main>
+      </SidebarInset>
     </SidebarProvider>
   );
 }
